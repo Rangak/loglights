f861e5d1675d0d92409c01669edf25f1e6033250
Ranga Krishnan <rk@selwastor.com>
2017-05-15 22:03:08 -0700

    Added sorting by package and filtering

--- loglights.py
+++ loglights.py
@@ -15,7 +15,12 @@ def valid_date(datestring):
 parser = argparse.ArgumentParser(description='Extract Hyperledger Fabric Logfile Highlights')
 parser.add_argument('logfile', help='Name of Fabric logfile')
 parser.add_argument("-d", "--depth", type=int, default=1, help='Number of lines of event to show')
-parser.add_argument("-p", "--precision", type=int, default=2, help='Timestamp precision, sub second decimal places')
+parser.add_argument("-n", "--num", type=int, default=2, help='Number of sub second decimal places in timestamp')
+feature_parser = parser.add_mutually_exclusive_group(required=False)
+feature_parser.add_argument('-s', '--sort', dest='sort', action='store_true', help='Sort by package')
+feature_parser.add_argument('--no-sort', dest='sort', action='store_false',help='Sort by timestamp. This is the default')
+parser.set_defaults(sort=False)
+parser.add_argument('-p', '--packages', help='Filter to this list of comma separated packages', type=str, default='all')
 
 args = parser.parse_args()
 logfile = open(args.logfile, 'r')
@@ -23,6 +28,8 @@ logfile = open(args.logfile, 'r')
 num_lines=0
 num_events=0
 prev =[0,0]
+
+output = []
 
 for line in logfile:
     if valid_date(line[:10]):
@@ -35,7 +42,7 @@ for line in logfile:
             package = body.split(']')[0].split('[')[2]
         else:
             continue
-        tsend = 20 + args.precision
+        tsend = 20 + args.num
         ts = line[11:tsend]
         num_lines += 1
         if prev == [ts, package]:
@@ -44,10 +51,21 @@ for line in logfile:
             if ts != prev[0]:
                 if text[-1:] == '\n':
                     text = text[0:-1]
-                output =  [ts, package, text]
-                print('{:14} {:20} {:.140}'.format(*output))
+                output.append([ts, package, text])
                 num_events += 1               
             prev = [ts,package]        
+
+if args.sort:
+    output.sort(key=lambda x: x[1])
+
+if args.packages!='all':
+    package_filter = args.packages.split(',')
+    for line in output:
+        if line[1] in package_filter:
+            print('{:14} {:20} {:.140}'.format(*line))
+else:
+    for line in output:
+        print('{:14} {:20} {:.140}'.format(*line))
 
 print("NumLines :" + str(num_lines))
 print("NumEvents :" + str(num_events))
